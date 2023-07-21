6c5df15a3b112ba6ee23bb651f9af03b3dfc876f
Giane <giane.teo@gmail.com>
2023-07-21 14:01:22 +0200

    First commit

---
+++ strat.py
@@ -0,0 +1,21 @@
+import logging
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+
+TOKEN = '6325210419:AAHvIBN5bd5C4wakkvtpyP034FOsE4K3lIw'
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+if __name__ == '__main__':
+    application = ApplicationBuilder().token(TOKEN).build()
+    
+    start_handler = CommandHandler('start', start)
+    application.add_handler(start_handler)
+    
+    application.run_polling()
