7a5000ae81e1040890f4351b3419439ce35a9b9f
Giane <giane.teo@gmail.com>
2023-07-24 13:39:26 +0200

    Added message handler

--- strat.py
+++ strat.py
@@ -1,6 +1,6 @@
 import logging
 from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters
 
 TOKEN = '6325210419:AAHvIBN5bd5C4wakkvtpyP034FOsE4K3lIw'
 
@@ -13,12 +13,16 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
 async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text='Hi %s how can I help you'%update.message.from_user.full_name)
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=f'Hi {update.message.from_user.full_name} how can I help you')
+
+async def chat(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("I reply to your message")
 
 if __name__ == '__main__':
     application = ApplicationBuilder().token(TOKEN).build()
     
     application.add_handler(CommandHandler('start', start))
     application.add_handler(CommandHandler('help', help))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, chat))
     
     application.run_polling()
